24224d7dba4f5563a77275a74be34611125fc741
jayl00 <hyinky@gmail.com>
2024-09-19 19:04:31 +0900

    vite_setting

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import Header from './header.jsx'
-import Posting from './posting.jsx'
-import List from './list-main.jsx'
-// import './index.css'
-import './global.css'
+import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <Posting />
-      <List />
+      <App />
   </StrictMode>,
 )
 
