42bb5da96e6f08a3a9678a0c2bc5feeb61b11195
jayl00 <hyinky@gmail.com>
2024-09-23 20:25:26 +0900

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import { useState } from 'react';
-// import Header from './header.jsx'
 import Post from './post.jsx'
 import './post.css'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      {/*<Header />*/}
       <Post />
   </StrictMode>,
 )
