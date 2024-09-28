41d48c0db44825b7967050fa28d1d920cb753e7c
jayl00 <hyinky@gmail.com>
2024-09-28 16:02:58 +0900

    eslint-setting

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,12 @@ import pluginReact from "eslint-plugin-react";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {languageOptions: {globals: {...globals.browser, ...globals.node}}},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules:{
+      "react/react-in-jsx-scope": "off"}},
+
 ];
+
